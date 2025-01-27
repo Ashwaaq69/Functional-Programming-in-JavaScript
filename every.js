816845401478289df3655f9ea21bc18c6ee39ebf
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2025-01-27 19:33:32 +0300

    every method

---
+++ every.js
@@ -0,0 +1,16 @@
+// The method every is somehow similar to some but every item must satisfy the criteria. The method every like some returns a boolean.
+
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const allAreEvens = numbers.every((n) => n % 2 === 0)
+const allAreOdds= numbers.every((n) => n % 2 !== 0)
+
+console.log(allAreEvens) // false
+console.log(allAreOdds)  // false
+
+const evens = [0, 2, 4, 6, 8, 10]
+const someAreEvens = evens.some((n) => n % 2 === 0)
+const someAreOdds = evens.some((n) => n % 2 !== 0)
+
+console.log(someAreEvens) // true
+console.log(someAreOdds)  // false
